71389f98d80f0fecef283bf2ab7a44a0434c2448
LeafNewGame <youseitou2@gmail.com>
2026-06-15 00:40:21 +0900

    JavaScript勉強開始

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 let userName = "Leaf";
 
 console.log(userName);
+
+let price = 1000;
+let quantity = 3;
+
+let total = price * quantity;
+
+console.log(total);
+
+let inStock = true
+if(inStock){
+    console.log("購入可能");
+}
+else{
+    console.log("購入不可");
+}
+
+let cart = [
+    {
+        name:"PC",
+        price:90000
+    },
+    {
+        name:"table",
+        price:60000
+    }
+];
+
+function calculateTotal(){
+
+    let total = 0;
+
+    total += cart[0].price;
+    total += cart[1].price;   
+    return total;
+};
+console.log(calculateTotal());
+
+
+
+let score = 89;
+
+if(score>=90){
+    console.log("A");
+}else if(score>=80){
+    console.log("B");
+}else if(score>=70){
+    console.log("C");
+}else if(score>=60){
+    console.log("D");
+}else{
+    console.log("E");
+};
