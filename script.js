52d0f513283a014c25d04e37628e4d21a5ab9d8f
LeafNewGame <youseitou2@gmail.com>
2026-06-15 23:43:42 +0900

    ボタン機能学習

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
-let title = document.getElementById("title");
-let btn = document.getElementById("btn");
+let count = 0;
 
-btn.onclick = function(){
-    title.textContent = "ボタンが押されました";
+let countText =
+document.getElementById("countText");
+
+let plusBtn =
+document.getElementById("plusBtn");
+
+let minusBtn =
+document.getElementById("minusBtn");
+
+let reset = document.getElementById("reset");
+
+plusBtn.addEventListener(
+    "mouseover",
+    function(){
+        count += 1
+        countText.textContent = count;
+    }
+);
+
+minusBtn.onclick = function(){
+
+    count--;
+
+    countText.textContent = count;
+
 };
+
+reset.onclick = function(){
+    count = 0
+    countText.textContent = count;
+};
+
+
+
+let nameBox =
+document.getElementById("nameBox");
+
+let btn =
+document.getElementById("btn");
+
+let result =
+document.getElementById("result");
+
+btn.addEventListener(
+    "click",
+    function(){
+        console.log(nameBox.value);
+
+        result.textContent = nameBox.value;
+
+    }
+);
