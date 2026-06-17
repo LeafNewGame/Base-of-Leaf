028f58994e87fbf9531d22d6d7aa6a3df9659d44
LeafNewGame <youseitou2@gmail.com>
2026-06-17 20:24:59 +0900

    header追加

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+
+
 const menu = document.getElementById("menu");
 const btn = document.querySelector(".menu-btn");
-const closeBtn = document.getElementById("close-btn");
 
 btn.addEventListener("click", () => {
-    menu.classList.add("active");
-    btn.classList.add("active");
-});
 
-closeBtn.addEventListener("click", () => {
-    menu.classList.remove("active");
-    btn.classList.remove("active");
+    menu.classList.toggle("active");
+    btn.classList.toggle("active");
+
 });
