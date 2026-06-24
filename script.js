d187fdfe6d1ef8ac180d3980c8722b035a3e8ae0
LeafNewGame <youseitou2@gmail.com>
2026-06-24 15:04:08 +0900

    トプ画追加

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ btn.addEventListener("click", () => {
 const track = document.querySelector(".slide-track");
 
 track.innerHTML += track.innerHTML;
+
+
+const overlay =
+document.querySelector(".hero-overlay");
+
+window.addEventListener(
+    "scroll",
+    function(){
+
+        let scrollY =
+        window.scrollY;
+
+        let opacity =
+        scrollY / 800;
+
+        if(opacity > 0.7){
+            opacity = 0.7;
+        }
+
+        overlay.style.opacity =
+        opacity;
+
+    }
+);
